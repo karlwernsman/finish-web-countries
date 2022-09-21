d2dab2a61debeb413f935e1c41d737b641ab6933
karl wernsman <wernsmank@gmail.com>
2022-09-21 14:32:33 -0700

    Step C - search functioning

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ window.addEventListener('load', async () => {
 
 async function findCountries(name, continent) {
     // > Part A: Call the service function that gets the countries
-    const response = await getCountries();
+    const response = await getCountries(name, continent);
     // > Part C: Add the name and continent arguments to getCountries
 
     // > Part A: Assign to state the :
@@ -57,6 +57,7 @@ searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(searchForm);
     // > Part C: Call findCountries with name and continent from formData
+    findCountries(formData.get('name'), formData.get('continent'));
 });
 
 /* Display Functions */
